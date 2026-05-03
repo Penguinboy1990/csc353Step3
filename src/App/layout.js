2490fd3609fd7007301108d55bfa4f726765ce0a
Penguinboy1990 <102927508+Penguinboy1990@users.noreply.github.com>
2026-05-03 08:03:36 -0400

    Update layout.js

--- src/App/layout.js
+++ src/App/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { CartProvider } from './context/CartContext';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +25,11 @@ export default function RootLayout({ children }) {
    >
       <body
         className="min-h-full flex flex-col"
+        suppressHydrationWarning
       >
-        {children}
+      <CartProvider>
+          {children}
+      </CartProvider>
       </body>
     </html>
   );
